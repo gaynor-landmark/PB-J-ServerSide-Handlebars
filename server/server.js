8050d602e105f3975006ddaa91e29992f15f3700
gaynor-landmark <g.l.landmark@gmail.com>
2016-02-28 12:52:03 +1300

    server-side load of the Peanut Butter and Honey page

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@
 
 var express = require('express')
 var exphbs = require('express-handlebars')
+var fs = require('fs')
+var path = require('path')
 
 
 
@@ -16,23 +18,23 @@ app.get('/', function (req, res) {
 
 
 // GET
-// setup the route for the peanutButter
-// app.get('/', function (req, res) {
-//   var fullPath = (__dirname + '/../db/db.json')   //fs needs a full path
-//   console.log(fullPath)
-//   fs.readFile(fullPath, 'utf8', function(err, data) {
-//     if (err) {
-//       console.log(err, 'err')
-//     }
-//     else {
-//       var fileJson = JSON.parse(data)
-//       console.log(fileJson)
-//       //res.json(fileJson.peanutButter)
-//       res.render('home')
-//     }
-//   })
-//
-// })
+//setup the route for the peanutButter
+app.get('/peanutButter', function (req, res) {
+  var fullPath = (__dirname + '/../db/db.json')   //fs needs a full path
+  console.log(fullPath)
+  fs.readFile(fullPath, 'utf8', function(err, data) {
+    if (err) {
+      console.log(err, 'err')
+    }
+    else {
+      var fileJson = JSON.parse(data)
+      console.log(fileJson)
+      //res.json(fileJson.peanutButter)
+      res.render('peanutButter', fileJson)
+    }
+  })
+
+})
 
 
 
