c8f43c72c0e847c6e128ec2b730e81b81eed29ed
gaynor-landmark <g.l.landmark@gmail.com>
2016-02-28 11:47:02 +1300

    basic handlebars working

---
+++ server/server.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var express = require('express')
+var exphbs = require('express-handlebars')
+
+
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+
+// GET
+// setup the route for the peanutButter
+// app.get('/', function (req, res) {
+//   var fullPath = (__dirname + '/../db/db.json')   //fs needs a full path
+//   console.log(fullPath)
+//   fs.readFile(fullPath, 'utf8', function(err, data) {
+//     if (err) {
+//       console.log(err, 'err')
+//     }
+//     else {
+//       var fileJson = JSON.parse(data)
+//       console.log(fileJson)
+//       //res.json(fileJson.peanutButter)
+//       res.render('home')
+//     }
+//   })
+//
+// })
+
+
+
+
+module.exports = app
